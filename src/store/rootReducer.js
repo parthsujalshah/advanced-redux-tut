663857be680167baa40e80b5f66a6759db062ede
parthsujalshah <parthsujalshah@gmail.com>
2020-05-26 14:46:23 +0530

    added user reducer

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux';
 import cakeReducer from './reducers/cake';
 import iceCreamReducer from './reducers/iceCream';
+import userReducer from './reducers/user';
 
 const rootReducer = combineReducers({
   cake: cakeReducer,
-  iceCream: iceCreamReducer
+  iceCream: iceCreamReducer,
+  user: userReducer
 });
 
 export default rootReducer
